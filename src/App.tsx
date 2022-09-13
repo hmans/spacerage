274d4f255da93a5ad182c04b5184ff2d0533bb32
Hendrik Mans <hendrik@mans.de>
2022-09-13 18:06:30 +0200

    Add god rays

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,17 @@ export const App = () => (
     <Loader />
     <RenderCanvas>
       <RenderPipeline
-        bloom={{ luminanceThreshold: 0.9, intensity: 10 }}
+        bloom={{ luminanceThreshold: 0.9, intensity: 2 }}
         antiAliasing
         vignette
+        godRays={{
+          density: 0.96,
+          decay: 200.2,
+          weight: 1.3,
+          exposure: 0.54,
+          samples: 60,
+          clampMax: 0.1
+        }}
       >
         <Suspense>
           <GameState.Match state="menu">
--- src/scenes/menu/MenuScene.tsx
+++ src/scenes/menu/MenuScene.tsx
@@ -1,31 +1,50 @@
-import { PerspectiveCamera } from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
+import { composable, Layer, modules } from "material-composer-r3f"
+import { useLayoutEffect } from "react"
+import { useRenderPipeline } from "render-composer"
+import { Color } from "three"
 import { Skybox } from "../../common/Skybox"
 import { Animate, float, rotate } from "../../lib/animation-composer/Animate"
 import { AsteroidField } from "./AsteroidField"
 import { AsteroidBelt } from "./vfx/AsteroidBelt"
 import { Dust } from "./vfx/Dust"
 
-export const MenuScene = () => (
-  <group>
-    <ambientLight intensity={0.1} />
-    <directionalLight position={[30, 0, -30]} intensity={2} />
+export const MenuScene = () => {
+  const { sun } = useRenderPipeline()
 
-    <Animate position={[0, 0, 10]} fun={rotate(0, -0.01, 0)}>
-      <PerspectiveCamera makeDefault />
-    </Animate>
+  useLayoutEffect(() => {
+    sun.position.set(1200, 50, -1000)
+    sun.scale.setScalar(150)
+  }, [])
 
-    <Dust />
-    <Skybox />
-    {/* <AsteroidField /> */}
-    <AsteroidBelt position={[30, 0, -30]} rotation={[0.6, 0, -0.2]} />
+  return (
+    <group>
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[30, 0, -30]} intensity={2} />
 
-    {/* <Animate fun={float([1, 2, 3])}>
-      <Animate fun={rotate(1, 1.5, -0.5)}>
-        <mesh>
-          <dodecahedronGeometry />
-          <meshStandardMaterial color="red" metalness={0.5} roughness={0.6} />
-        </mesh>
+      <Animate position={[0, 0, 10]} fun={rotate(0, -0.02, 0)}>
+        <PerspectiveCamera makeDefault />
       </Animate>
-    </Animate> */}
-  </group>
-)
+
+      <Dust />
+      <Skybox />
+
+      <group position={[30, 0, -30]} rotation={[0.6, 0, -0.2]}>
+        <Animate fun={rotate(0.1, 0.15, -0.05)}>
+          <mesh scale={9}>
+            <sphereGeometry />
+            <composable.meshStandardMaterial
+              color="brown"
+              metalness={0.5}
+              roughness={0.6}
+            >
+              <modules.Color color={new Color("#754")} />
+              {/* <modules.Fresnel /> */}
+            </composable.meshStandardMaterial>
+          </mesh>
+        </Animate>
+        <AsteroidBelt />
+      </group>
+    </group>
+  )
+}
