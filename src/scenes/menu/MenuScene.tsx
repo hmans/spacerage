9537119c1875ad6ec23bcfd85bbf524e7c0ef2c3
Hendrik Mans <hendrik@mans.de>
2022-09-13 20:07:51 +0200

    Yoooo

--- src/scenes/menu/MenuScene.tsx
+++ src/scenes/menu/MenuScene.tsx
@@ -1,13 +1,21 @@
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei"
-import { composable, Layer, modules } from "material-composer-r3f"
+import { PerspectiveCamera } from "@react-three/drei"
+import { composable, modules } from "material-composer-r3f"
 import { useLayoutEffect } from "react"
 import { useRenderPipeline } from "render-composer"
-import { Color } from "three"
+import { Vec3 } from "shader-composer"
+import { Color, Quaternion, Vector3 } from "three"
 import { Skybox } from "../../common/Skybox"
-import { Animate, float, rotate } from "../../lib/animation-composer/Animate"
-import { AsteroidField } from "./AsteroidField"
+import {
+  Animate,
+  AnimateUpdateCallback,
+  rotate
+} from "../../lib/animation-composer/Animate"
 import { AsteroidBelt } from "./vfx/AsteroidBelt"
 import { Dust } from "./vfx/Dust"
+import { Nebula } from "./vfx/Nebula"
+
+const tmpVec3 = new Vector3()
+const tmpQuat = new Quaternion()
 
 export const MenuScene = () => {
   const { sun } = useRenderPipeline()
@@ -17,32 +25,49 @@ export const MenuScene = () => {
     sun.scale.setScalar(150)
   }, [])
 
+  const animateCamera: AnimateUpdateCallback = (g, dt, { camera }) => {
+    rotate(0, -0.05, 0)(g, dt)
+    // camera.getWorldQuaternion(tmpQuat)
+    // tmpVec3.set(0, 0, -2 * dt).applyQuaternion(tmpQuat)
+    // g.position.add(tmpVec3)
+    // g.rotation.z += dt * 0.01
+  }
+
   return (
     <group>
       <ambientLight intensity={0.1} />
       <directionalLight position={[30, 0, -30]} intensity={2} />
 
-      <Animate position={[0, 0, 10]} fun={rotate(0, -0.02, 0)}>
+      <Animate position={[0, 0, 10]} fun={animateCamera}>
         <PerspectiveCamera makeDefault />
       </Animate>
+
+      <Nebula
+        dimensions={Vec3([50, 20, 50])}
+        amount={300}
+        minSize={5}
+        maxSize={20}
+        opacity={0.1}
+      />
 
       <Dust />
       <Skybox />
 
       <group position={[30, 0, -30]} rotation={[0.6, 0, -0.2]}>
-        <Animate fun={rotate(0.1, 0.15, -0.05)}>
-          <mesh scale={9}>
-            <sphereGeometry />
-            <composable.meshStandardMaterial
-              color="brown"
-              metalness={0.5}
-              roughness={0.6}
-            >
-              <modules.Color color={new Color("#754")} />
-              {/* <modules.Fresnel /> */}
-            </composable.meshStandardMaterial>
-          </mesh>
-        </Animate>
+        <Nebula dimensions={Vec3([50, 5, 50])} amount={300} opacity={0.2} />
+
+        <mesh scale={9}>
+          <sphereGeometry />
+
+          <composable.meshStandardMaterial
+            color="brown"
+            metalness={0.5}
+            roughness={0.6}
+          >
+            <modules.Color color={new Color("#754")} />
+          </composable.meshStandardMaterial>
+        </mesh>
+
         <AsteroidBelt />
       </group>
     </group>
