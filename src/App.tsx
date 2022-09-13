86335e86f7ae37f15f890509f84914bf250ee379
Hendrik Mans <hendrik@mans.de>
2022-09-13 09:21:41 +0200

    Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
+import { Loader } from "@react-three/drei"
+import { Suspense } from "react"
 import { RenderCanvas, RenderPipeline } from "render-composer"
 import { MenuScene } from "./scenes/menu/MenuScene"
 import { GameState } from "./state"
 
 export const App = () => (
-  <RenderCanvas>
-    <RenderPipeline
-      bloom={{ luminanceThreshold: 0.2, intensity: 10 }}
-      antiAliasing
-      vignette
-    >
-      <GameState.Match state="menu">
-        <MenuScene />
-      </GameState.Match>
-    </RenderPipeline>
-  </RenderCanvas>
+  <>
+    <Loader />
+    <RenderCanvas>
+      <RenderPipeline
+        bloom={{ luminanceThreshold: 0.2, intensity: 10 }}
+        antiAliasing
+        vignette
+      >
+        <Suspense>
+          <GameState.Match state="menu">
+            <MenuScene />
+          </GameState.Match>
+        </Suspense>
+      </RenderPipeline>
+    </RenderCanvas>
+  </>
 )
