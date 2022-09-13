b42891c18af012f19dcce6a8b15cb3ab18ec3d69
Hendrik Mans <hendrik@mans.de>
2022-09-13 15:33:21 +0200

    Cleanup

--- src/scenes/menu/MenuScene.tsx
+++ src/scenes/menu/MenuScene.tsx
@@ -9,22 +9,21 @@ export const MenuScene = () => (
     <ambientLight intensity={0.1} />
     <directionalLight position={[30, 0, -30]} intensity={2} />
 
-    <Dust />
-    <Skybox />
-
     <Animate position={[0, 0, 10]} fun={rotate(0, -0.01, 0)}>
       <PerspectiveCamera makeDefault />
     </Animate>
 
-    <Animate fun={float([1, 2, 3])}>
+    <Dust />
+    <Skybox />
+    <AsteroidField />
+
+    {/* <Animate fun={float([1, 2, 3])}>
       <Animate fun={rotate(1, 1.5, -0.5)}>
         <mesh>
           <dodecahedronGeometry />
           <meshStandardMaterial color="red" metalness={0.5} roughness={0.6} />
         </mesh>
       </Animate>
-    </Animate>
-
-    <AsteroidField />
+    </Animate> */}
   </group>
 )
