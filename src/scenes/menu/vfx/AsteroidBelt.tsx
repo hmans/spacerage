9a7d5e8f2290c6090069e6eb0fb9b186eeb1901a
Hendrik Mans <hendrik@mans.de>
2022-09-14 07:00:06 +0200

    Better belt!

--- src/scenes/menu/vfx/AsteroidBelt.tsx
+++ src/scenes/menu/vfx/AsteroidBelt.tsx
@@ -1,9 +1,6 @@
 import { useGLTF } from "@react-three/drei"
-import { Material, Mesh } from "three"
-import { InstanceSetupCallback } from "vfx-composer"
-import { Emitter, Particles, useParticles } from "vfx-composer-r3f"
-import { composable, modules } from "material-composer-r3f"
 import { GroupProps } from "@react-three/fiber"
+import { composable, Layer, modules } from "material-composer-r3f"
 import {
   $,
   Add,
@@ -17,12 +14,42 @@ import {
   Rotation3D,
   Rotation3DY,
   ScaleAndOffset,
-  Sub,
   Vec3
 } from "shader-composer"
 import { Random } from "shader-composer-toybox"
+import { DoubleSide, Material, Mesh } from "three"
+import { InstanceSetupCallback } from "vfx-composer"
+import { Emitter, Particles } from "vfx-composer-r3f"
 
-export const AsteroidBelt = (props: GroupProps) => {
+export const AsteroidBelt = (props: GroupProps) => (
+  <group {...props}>
+    <SmallAsteroids amount={100_000} />
+    <LargeAsteroids amount={10_000} />
+  </group>
+)
+
+const SmallAsteroids = ({ amount = 10_000 }: { amount?: number }) => {
+  const id = Float(InstanceID, { varying: true })
+  const random = (offset: Input<"float">) =>
+    Random($`${offset} + ${id} * 1.1005`)
+  const setup: InstanceSetupCallback = () => {}
+
+  return (
+    <Particles capacity={amount}>
+      <planeGeometry />
+
+      <composable.meshStandardMaterial side={DoubleSide} color="#000">
+        <modules.Scale scale={ScaleAndOffset(random(0.1), 0.1, 0.01)} />
+        <BeltModules id={id} height={12} />
+      </composable.meshStandardMaterial>
+
+      {/* Spawn 10.000 of them! */}
+      <Emitter limit={amount} rate={Infinity} setup={setup} />
+    </Particles>
+  )
+}
+
+const LargeAsteroids = ({ amount = 10_000 }: { amount?: number }) => {
   const gltf = useGLTF("/models/asteroid03.gltf")
   const mesh = gltf.scene.children[0] as Mesh
 
@@ -32,40 +59,72 @@ export const AsteroidBelt = (props: GroupProps) => {
 
   const setup: InstanceSetupCallback = () => {}
 
+  const rotationAxis = ScaleAndOffset(
+    Vec3([random(12), random(84), random(1)]),
+    2,
+    -1
+  )
+
   return (
-    <group {...props}>
-      <Particles geometry={mesh.geometry} capacity={10_000}>
-        <composable.material instance={mesh.material as Material}>
-          {/* Rotate the asteroid */}
-          <modules.Rotate
-            rotation={Mat3(
-              Rotation3D(
-                Vec3([random(12), random(84), random(1)]),
-                Mul(GlobalTime, Sub(random(-5), 0.5))
-              )
-            )}
-          />
+    <Particles geometry={mesh.geometry} capacity={amount}>
+      <composable.material instance={mesh.material as Material}>
+        <RotateOverTime
+          axis={rotationAxis}
+          speed={ScaleAndOffset(random(-5), 2, -1)}
+        />
+        <modules.Scale scale={ScaleAndOffset(Pow(random(1), 3), 0.3, 0.1)} />
+        <BeltModules id={id} height={6} />
+      </composable.material>
 
-          {/* Scale the asteroid */}
-          <modules.Scale scale={ScaleAndOffset(Pow(random(1), 3), 0.3, 0.1)} />
+      <Emitter limit={amount} rate={Infinity} setup={setup} />
+    </Particles>
+  )
+}
 
-          {/* Apply a random offset (position) */}
-          <modules.Translate
-            offset={Vec3([Add(Mul(random(2), 40), 15), Mul(random(123), 2), 0])}
-          />
+const RotateOverTime = ({
+  axis = Vec3([0, 0, 1]),
+  speed = 1
+}: {
+  axis: Input<"vec3">
+  speed?: Input<"float">
+}) => (
+  <modules.Rotate rotation={Mat3(Rotation3D(axis, Mul(GlobalTime, speed)))} />
+)
 
-          {/* Distribute asteroids radially */}
-          <modules.Rotate rotation={Rotation3DY(Mul(random(5), Math.PI * 2))} />
+type BeltProps = {
+  id: Input<"float">
+  width?: Input<"float">
+  height?: Input<"float">
+  distance?: Input<"float">
+}
 
-          {/* Rotate everything over time */}
-          <modules.Rotate
-            rotation={Rotation3DY(Mul(GlobalTime, Mul(random(12), 0.05)))}
-          />
-        </composable.material>
+const BeltModules = ({
+  id,
+  width = 40,
+  distance = 15,
+  height = 5
+}: BeltProps) => {
+  const random = (offset: Input<"float">) =>
+    Random($`${offset} + ${id} * 1.1005`)
 
-        {/* Spawn 10.000 of them! */}
-        <Emitter limit={10_000} rate={Infinity} setup={setup} />
-      </Particles>
-    </group>
+  return (
+    <Layer>
+      {/* Apply a random offset (position) */}
+      <modules.Translate
+        offset={Vec3([
+          Add(Mul(random(0.2), width), distance),
+          ScaleAndOffset(random(0.3), height, Mul(height, -0.5)),
+          0
+        ])}
+      />
+
+      {/* Distribute asteroids radially */}
+      <modules.Rotate rotation={Rotation3DY(Mul(random(0.4), Math.PI * 2))} />
+
+      {/* Rotate everything over time */}
+      <modules.Rotate
+        rotation={Rotation3DY(Mul(GlobalTime, Mul(random(0.25), 0.05)))}
+      />
+    </Layer>
   )
 }
